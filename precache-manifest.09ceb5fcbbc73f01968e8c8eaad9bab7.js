012a42af440437d089ce716699ea740a893b1bfb
hitman99 <hitman99@users.noreply.github.com>
2020-09-30 19:08:44 +0000

    deploy: 56fbf2bf40fb60ad02eb406795564bef133e1874

---
+++ precache-manifest.09ceb5fcbbc73f01968e8c8eaad9bab7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93c8951831e5e7fc8a101eea0a014f3",
+    "url": "/kubernetes-rbac-helper/index.html"
+  },
+  {
+    "revision": "35e58e021e31a2390542",
+    "url": "/kubernetes-rbac-helper/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "935501dc1f43ca418634",
+    "url": "/kubernetes-rbac-helper/static/js/2.66969df3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kubernetes-rbac-helper/static/js/2.66969df3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35e58e021e31a2390542",
+    "url": "/kubernetes-rbac-helper/static/js/main.a2ae8c5e.chunk.js"
+  },
+  {
+    "revision": "39b5abfe6a8a90d00265",
+    "url": "/kubernetes-rbac-helper/static/js/runtime-main.71ef19be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kubernetes-rbac-helper/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aa139cc9678955ab298d410b02d5ede5.js"
+  "/kubernetes-rbac-helper/precache-manifest.09ceb5fcbbc73f01968e8c8eaad9bab7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kubernetes-rbac-helper/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
